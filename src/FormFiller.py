20b47ac76f98d5eb5986b3b4c08a05e3b388b9a4
maksym <01178511@pw.edu.pl>
2024-02-16 17:28:41 +0100

    remove concatenate_classname from formfiller since it's handled in util + remove get_all_questions implementation for now

--- src/FormFiller.py
+++ src/FormFiller.py
@@ -3,7 +3,9 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from src.constants import *
-from src.util import delay, concatenate_classname, Question
+from src.util import delay
+from src.classnames import GOOGLE_FORM_QUESTION_CARD_CLASS, GOOGLE_FORM_SUBMIT_BUTTON_CLASS
+from src.Question import Question
 
 
 class FormFiller(Firefox):
@@ -28,23 +30,18 @@ class FormFiller(Firefox):
     def get_all_questions(self) -> list[Question]:
         """Finds all questions in a form"""
         question_cards = self.__find_elements_by_classname(GOOGLE_FORM_QUESTION_CARD_CLASS)
-        # Find label and input elements for each card TODO: radio inputs etc
-        label_elements = [card.find_element(by=By.CLASS_NAME, value=GOOGLE_FORM_QUESTION_LABEL_CLASS)
-                          for card in question_cards]
-        input_elements = [card.find_element(by=By.TAG_NAME, value="input") for card in question_cards]
-
-        return [Question(label_elem, input_elem, input_elem.accessible_name) for label_elem, input_elem in
-                zip(label_elements, input_elements)]
+        # todo: implement
+        return []
 
     def __find_elements_by_classname(self, classname: str) -> list[WebElement]:
         """Finds elements by the given classname, basically a wrapper which takes care of
         choosing the correct By and formatting the classname"""
-        return self.find_elements(by=By.CLASS_NAME, value=concatenate_classname(classname))
+        return self.find_elements(by=By.CLASS_NAME, value=classname)
 
     def find_element_by_classname(self, classname: str) -> WebElement:
         """Finds an element by the given classname, basically a wrapper which takes care of
         choosing the correct By and formatting the classname"""
-        return self.find_element(by=By.CLASS_NAME, value=concatenate_classname(classname))
+        return self.find_element(by=By.CLASS_NAME, value=classname)
 
     def __find_submit_button(self) -> WebElement:
         """Finds the form's submit button"""
