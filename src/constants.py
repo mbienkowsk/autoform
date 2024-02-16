52c467d043846b791147dc96577fba8e70ad8e5d
maksym <01178511@pw.edu.pl>
2024-02-16 12:57:46 +0100

    add constants for select and the test form

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,12 @@ T = 2  # delay for slowdown functions
 FORM1 = "https://docs.google.com/forms/d/e/1FAIpQLSeoomCweaXmmwaUZR3O9UrPOydlfdZt2wmFSlGqC0W6_TGVnQ/viewform"
 FORM2 = "https://docs.google.com/forms/d/e/1FAIpQLSeuyhxget0MZs-yKuRaDrHODZHVJsjw4OBaRQb-nq5Bovv3Ug/viewform"
 FORM3 = "https://docs.google.com/forms/d/e/1FAIpQLSfCxr7-TcD_HwZnam4Kzh2qWfYMcCdjOy7wfPZQE_5JlaDk4A/viewform"
+TEST_FORM = "https://forms.gle/FQK2kEadG8CMtL3c9"
 
 GOOGLE_FORM_SUBMIT_BUTTON_CLASS = "NPEfkd RveJvd snByac"
 GOOGLE_FORM_QUESTION_CARD_CLASS = "Qr7Oae"
 GOOGLE_FORM_QUESTION_LABEL_CLASS = "M7eMe"
 GOOGLE_FORM_RADIO_INPUT_CLASS = "Od2TWd hYsg7c"
+GOOGLE_FORM_SELECT_CLASS = "ry3kXd"
+GOOGLE_FORM_SELECT_OPTION_CLASS = "MocG8c HZ3kWc mhLiyf OIC90c LMgvRb"
+
