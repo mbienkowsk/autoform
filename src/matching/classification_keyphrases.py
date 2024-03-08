905cda0231d5f730b05387637be2a5765a07b03b
maksym <01178511@pw.edu.pl>
2024-03-08 16:09:40 +0100

    refactor classification_keyphrases.py, map questions to classifications

--- src/matching/classification_keyphrases.py
+++ src/matching/classification_keyphrases.py
@@ -13,14 +13,9 @@ class QuestionClassification:
     person: PersonRegardedByQuestion | None
 
 
-TEAM_NAME_KEYPHRASES = [
-    "nazwa drużyny"
-]
-
-TEAM_NAME_QUESTIONS = [
-    "nazwa drużyny"
-]
-
+# DATA COLLECTION
+TEAM_NAME_KEYPHRASES = ["nazwa drużyny"]
+TEAM_NAME_QUESTIONS = ["nazwa drużyny"]
 TEAM_NAME_CLASSIFICATION = QuestionClassification(QuestionTopic.TEAM_NAME, PersonRegardedByQuestion.NONE)
 
 DATA_COLLECTION_KEYPHRASES = [
@@ -30,10 +25,10 @@ DATA_COLLECTION_KEYPHRASES = [
     "dane osobowe",
     "podmioty trzecie"
 ]
-
 DATA_COLLECTION_CLASSIFICATION = QuestionClassification(QuestionTopic.AGREE_FOR_DATA_COLLECTION,
                                                         PersonRegardedByQuestion.NONE)
 
+# ANYTHING ELSE?
 ANYTHING_ELSE_KEYPHRASES = [
     "coś jeszcze",
     "nas poinformować",
@@ -41,18 +36,19 @@ ANYTHING_ELSE_KEYPHRASES = [
     "nam przekazać",
     "przekazać nam"
 ]
-
 ANYTHING_ELSE_QUESTIONS = [
     "czy jest coś o czym chcielibyście nas poinformować?",
     "czy chcecie nam przekazać coś jeszcze?"
 ]
-
 ANYTHING_ELSE_CLASSIFICATION = QuestionClassification(QuestionTopic.ANYTHING_ELSE, PersonRegardedByQuestion.NONE)
 
+
+# NAME AND SURNAME
 NAME_SURNAME_KEYPHRASES = [
     "imię i nazwisko",
     "nazwisko"
 ]
+
 # todo: either not make it frozen or define some addition operation to combine the person and question classif?
 NAME_SURNAME_CLASSIFICATION = QuestionClassification(QuestionTopic.NAME_SURNAME, PersonRegardedByQuestion.NONE)
 
@@ -62,18 +58,19 @@ NAME_SURNAME_FIRST_PERSON_QUESTIONS = [
 NAME_SURNAME_FIRST_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.NAME_SURNAME,
                                                                   PersonRegardedByQuestion.PERSON_ONE)
 
-NAME_SURNAME_SECOND_PERSON_QUESTIONS = [
-    "imię i nazwisko drugiej osoby",
-]
+NAME_SURNAME_SECOND_PERSON_QUESTIONS = ["imię i nazwisko drugiej osoby"]
+
 NAME_SURNAME_SECOND_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.NAME_SURNAME,
                                                                    PersonRegardedByQuestion.PERSON_TWO)
 
+# DEBATING STATUS
 DEBATING_STATUS_KEYPHRASES = [
     "status debatancki",
     "status novice",
     "novice"
 ]
 
+# EMAIL ADDRESS
 EMAIL_ADDRESS_KEYPHRASES = [
     "e-mail,"
     "email",
@@ -81,65 +78,40 @@ EMAIL_ADDRESS_KEYPHRASES = [
     "adres email"  # todo: regex
 ]
 
-PHONE_NUMBER_KEYPHRASES = [
-    "numer telefonu",
-    "nr telefonu"  # todo: regex
-]
-
-DIET_KEYPHRASES = [
-    "żywieniowe",
-    "dieta",
-    "dietetyczne"
-]
-
-CLUB_MEMBERSHIP_KEYPHRASES = [
-    "przynależność do klubu",
-    "przynależność klubowa",
-    "przynależność instytucjonalna",
-    "członkiem",
-    "członek"
-]
-
-FACEBOOK_PROFILE_KEYPHREASES = [
-    "fb",
-    "profil",
-    "facebook",
-    "facebooka"  # todo: maybe regex
-]
-
-PLACE_TO_SLEEP_KEYPHRASES = [
-    "nocleg",
-    "noclegowe",
-]
-
-REGARDS_FIRST_PERSON_KEYPHRASES = [
-    "pierwsza osoba",
-    "pierwszej osoby",
-    "pierwszą osobę"  # todo: regex
-]
-
-REGARDS_SECOND_PERSON_KEYPHRASES = [
-    "druga osoba",
-    "drugiej osoby",
-    "drugą osobę"
-]
-
-# --------------------------------------------------------------
-
-
 EMAIL_ADDRESS_FIRST_PERSON_QUESTIONS = [
     "adres email pierwszej osoby",
     "adres e-mail pierwszej osoby",
 ]
 EMAIL_ADDRESS_FIRST_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.EMAIL_ADDRESS,
                                                                    PersonRegardedByQuestion.PERSON_ONE)
-
 EMAIL_ADDRESS_SECOND_PERSON_QUESTIONS = [
     "adres email drugiej osoby",
     "adres e-mail drugiej osoby",
 ]
+
 EMAIL_ADDRESS_SECOND_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.EMAIL_ADDRESS,
                                                                     PersonRegardedByQuestion.PERSON_TWO)
+PHONE_NUMBER_KEYPHRASES = [
+    "numer telefonu",
+    "nr telefonu"  # todo: regex
+]
+
+PHONE_NUMBER_FIRST_PERSON_QUESTIONS = [
+    "numer telefonu pierwszej osoby",
+]
+PHONE_NUMBER_FIRST_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.PHONE_NUMBER,
+                                                                  PersonRegardedByQuestion.PERSON_ONE)
+PHONE_NUMBER_SECOND_PERSON_QUESTIONS = [
+    "numer telefonu drugiej osoby"
+]
+PHONE_NUMBER_SECOND_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.PHONE_NUMBER,
+                                                                   PersonRegardedByQuestion.PERSON_TWO)
+
+DIET_KEYPHRASES = [
+    "żywieniowe",
+    "dieta",
+    "dietetyczne"
+]
 
 DIET_FIRST_PERSON_QUESTIONS = [
     "preferencje żywieniowe pierwszej osoby",  # todo: regex - startswith?
@@ -150,6 +122,14 @@ DIET_SECOND_PERSON_QUESTIONS = [
     "preferencje żywieniowe drugiej osoby",  # todo: regex - startswith?
 ]
 DIET_SECOND_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.DIET, PersonRegardedByQuestion.PERSON_TWO)
+
+CLUB_MEMBERSHIP_KEYPHRASES = [
+    "przynależność do klubu",
+    "przynależność klubowa",
+    "przynależność instytucjonalna",
+    "członkiem",
+    "członek"
+]
 
 CLUB_MEMBERSHIP_FIRST_PERSON_QUESTIONS = [
     "przynależność klubowa pierwszej osoby"
@@ -165,6 +145,18 @@ CLUB_MEMBERSHIP_SECOND_PERSON_QUESTIONS = [
 CLUB_MEMBERSHIP_SECOND_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.CLUB_MEMBERSHIP,
                                                                       PersonRegardedByQuestion.PERSON_TWO)
 
+FACEBOOK_PROFILE_KEYPHRASES = [
+    "fb",
+    "profil",
+    "facebook",
+    "facebooka"  # todo: maybe regex
+]
+
+PLACE_TO_SLEEP_KEYPHRASES = [
+    "nocleg",
+    "noclegowe",
+]
+
 PLACE_TO_SLEEP_FIRST_PERSON_QUESTIONS = [
     "nocleg w przypadku pierwszej osoby"
 ]
@@ -177,5 +169,38 @@ PLACE_TO_SLEEP_SECOND_PERSON_QUESTIONS = [
 PLACE_TO_SLEEP_SECOND_PERSON_CLASSIFICATION = QuestionClassification(QuestionTopic.PLACE_TO_SLEEP,
                                                                      PersonRegardedByQuestion.PERSON_TWO)
 
-QUESTIONS_AND_KEYPHRASES_MAPPED = {
+REGARDS_FIRST_PERSON_KEYPHRASES = [
+    "pierwsza osoba",
+    "pierwszej osoby",
+    "pierwszą osobę"  # todo: regex
+]
+
+REGARDS_SECOND_PERSON_KEYPHRASES = [
+    "druga osoba",
+    "drugiej osoby",
+    "drugą osobę"
+]
+
+questions_to_classifications = {
+    # Maps questions to respective classifications; if a question is equal to any in the list, it gets classified
+    TEAM_NAME_QUESTIONS: TEAM_NAME_CLASSIFICATION,
+    ANYTHING_ELSE_QUESTIONS: ANYTHING_ELSE_CLASSIFICATION,
+    NAME_SURNAME_FIRST_PERSON_QUESTIONS: NAME_SURNAME_FIRST_PERSON_CLASSIFICATION,
+    NAME_SURNAME_SECOND_PERSON_QUESTIONS: NAME_SURNAME_SECOND_PERSON_CLASSIFICATION,
+    EMAIL_ADDRESS_FIRST_PERSON_QUESTIONS: EMAIL_ADDRESS_FIRST_PERSON_CLASSIFICATION,
+    EMAIL_ADDRESS_SECOND_PERSON_QUESTIONS: EMAIL_ADDRESS_SECOND_PERSON_CLASSIFICATION,
+    PHONE_NUMBER_FIRST_PERSON_QUESTIONS: PHONE_NUMBER_FIRST_PERSON_CLASSIFICATION,
+    PHONE_NUMBER_SECOND_PERSON_QUESTIONS: PHONE_NUMBER_SECOND_PERSON_CLASSIFICATION,
+    DIET_FIRST_PERSON_QUESTIONS: DIET_FIRST_PERSON_CLASSIFICATION,
+    DIET_SECOND_PERSON_QUESTIONS: DIET_SECOND_PERSON_CLASSIFICATION,
+    CLUB_MEMBERSHIP_FIRST_PERSON_QUESTIONS: CLUB_MEMBERSHIP_FIRST_PERSON_CLASSIFICATION,
+    CLUB_MEMBERSHIP_SECOND_PERSON_QUESTIONS: CLUB_MEMBERSHIP_SECOND_PERSON_CLASSIFICATION,
+    PLACE_TO_SLEEP_FIRST_PERSON_QUESTIONS: PLACE_TO_SLEEP_FIRST_PERSON_CLASSIFICATION,
+    PLACE_TO_SLEEP_SECOND_PERSON_QUESTIONS: PLACE_TO_SLEEP_SECOND_PERSON_CLASSIFICATION
+}
+
+keywords_to_classifications = {
+    TEAM_NAME_KEYPHRASES: TEAM_NAME_CLASSIFICATION,
+    DATA_COLLECTION_KEYPHRASES: DATA_COLLECTION_CLASSIFICATION,
+    ANYTHING_ELSE_KEYPHRASES: ANYTHING_ELSE_CLASSIFICATION,
 }
